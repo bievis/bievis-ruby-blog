808aa61e2cee9464920e7a287646ed54a40af166
Diego <solfamidas@gmail.com>
2012-09-06 12:05:08 +0200

    Proyecto Base de RoR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BievisRubyBlog::Application.config.secret_token = '07d68b2967e4c051a0b5544b656dbf6fc4c38242f3d6a7947c881871a5ada587ee7c725dd009bdd0da2b1efc14a0af5e3d11744249ec27fda8faee3b9d76b276'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BievisRubyBlog::Application.config.session_store :cookie_store, key: '_bievis-ruby-blog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BievisRubyBlog::Application.config.session_store :active_record_store
